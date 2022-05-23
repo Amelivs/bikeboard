d9d1abf79d71fd463254a2f095e9fd3f4254f3e9
Amelivs <erhart.guillaume@gmail.com>
2022-05-23 15:52:59 +0200

    don't hardcode direction services

--- src/app/core/services/direction.service.ts
+++ src/app/core/services/direction.service.ts
@@ -1,13 +1,5 @@
 import { Injectable } from '@angular/core';
-
-export interface DirectionProvider {
-    name: string;
-    url: string;
-    waypointsOnly: boolean;
-    separator: string;
-    waypointPrefix: string;
-    format: 'WKT' | 'GeoJSON';
-}
+import SeedingData from '../../../seeding.json';
 
 export interface DirectionResult {
     format: 'WKT' | 'GeoJSON';
@@ -19,27 +11,8 @@ export interface DirectionResult {
 })
 export class DirectionService {
 
-    private readonly providers: DirectionProvider[] = [
-        {
-            name: 'ign',
-            format: 'WKT',
-            waypointsOnly: false,
-            waypointPrefix: '',
-            separator: ';',
-            url: 'https://wxs.ign.fr/essentiels/itineraire/rest/route.json?origin={{ORIGIN}}&destination={{DESTINATION}}&waypoints={{WAYPOINTS}}&method=DISTANCE&graphName=Pieton'
-        },
-        {
-            name: 'geoapify',
-            format: 'GeoJSON',
-            waypointsOnly: true,
-            waypointPrefix: 'lonlat:',
-            separator: '|',
-            url: 'https://api.geoapify.com/v1/routing?waypoints={{WAYPOINTS}}&mode=bicycle&apiKey=1b48259b810e48ddb151889f9ea58db0'
-        }
-    ];
-
     public async getDirection(origin: number[], waypoints: number[][], destination: number[], providerName: string): Promise<DirectionResult> {
-        let provider = this.providers.find(p => p.name === providerName);
+        let provider = SeedingData.directionProviders.find(p => p.name === providerName);
         if (provider == null) {
             throw new Error(`Direction provider ${providerName} not found`);
         }
@@ -61,7 +34,7 @@ export class DirectionService {
             throw new Error(await res.text());
         }
         return {
-            format: provider.format,
+            format: provider.format as any,
             data: await res.json()
         };
     }
